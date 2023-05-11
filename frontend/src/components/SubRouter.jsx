bbada83fc0f61c705cd54f2792cefb191e36aaae
patrache <ssk5442@naver.com>
2023-05-12 05:49:41 +0900

    [feat]: document list page init

--- frontend/src/components/SubRouter.jsx
+++ frontend/src/components/SubRouter.jsx
@@ -4,16 +4,18 @@ import Banner from './Banner';
 import Main from './Main';
 import Footer from './Footer';
 import Notice from './Notice';
+import DocumentList from '../pages/documents/documentList';
 
 export default function SubRouter() {
     return (
         <div className='container'>
-        <Banner/>
-        <Routes>
-            <Route path='/' element={<Main/>}></Route>
-            <Route path='/notice' element={<Notice/>}></Route>
-        </Routes>
-        <Footer/>
+            <Banner/>
+            <Routes>
+                <Route path='/' element={<Main/>}></Route>
+                <Route path='/notice' element={<Notice/>}></Route>
+                <Route path='/documents' element={<DocumentList/>}></Route>
+            </Routes>
+            <Footer/>
         </div>
-      )
+    )
 }
