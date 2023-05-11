56900c99018329bb9cafbb1db786789b621a6e55
patrache <ssk5442@naver.com>
2023-05-12 04:19:31 +0900

    [chore]: folder name Component -> components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './css/App.css';
 import {BrowserRouter,Route,Routes} from "react-router-dom";
-import NotFound from './Component/NotFound';
-import SubRouter from './Component/SubRouter';
+import NotFound from './components/NotFound';
+import SubRouter from './components/SubRouter';
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/nova/*" element={<SubRouter/>}></Route>
-      <Route path="*" element={<NotFound />}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/nova/*" element={<SubRouter/>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
